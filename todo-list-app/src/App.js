a13222d6a98dc94bc6d0277ef27c1c7219c762eb
francisChigozie <francisc@hs-pforzheim.de>
2021-06-06 00:27:59 +0200

    todo-list-app

---
+++ todo-list-app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  const text = '[1] Walk the Dog ';
+  const text2 = '[2] Buy Ice cream ' ;
+  const text3 = '[3] Read a Book';
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+        <p>{text}</p>
+        <p>{text2}</p>
+        <p>{text3}</p>
+    </div>
+  );
+}
+export default App;
