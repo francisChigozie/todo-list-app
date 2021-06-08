0f4cad331ab912425cb98c8b8a1cf46e0d033406
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-08 05:57:34 +0200

    Adding handleSubmit func to AddTodo

--- todo-list-app/src/AddTodo.js
+++ todo-list-app/src/AddTodo.js
@@ -2,10 +2,18 @@
 
 function AddTodo () {
 
+    function handleSubmit (e) {
+
+        e.preventDefault()
+        console.log(e.target.elements.input.value)
+        e.target.elements.input.value = ''
+
+    }
+
     return (
-        <form>
-            <input type='text' />
-            <button type='submit'>Add</button>
+        <form onSubmit={handleSubmit}>
+            <input type='text' name='input' />
+            <button type='submit' >Add</button>
         </form>
 
     )
